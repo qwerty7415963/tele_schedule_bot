1c063e5dd18bd8500dd7fb76abe61054a4e5be90
DESKX300-PC\Admin <qwerty7415963@gmail.com>
2023-12-04 17:12:35 +0700

    + add express

--- bot.ts
+++ bot.ts
@@ -8,9 +8,14 @@ let unblockChatJob: any
 const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
 // Admin user IDs
-const ADMINS = [5544828606]
+const ADMINS = [5544828606, 6747207838]
+
+bot.command('check', (ctx) => ctx.reply('is alive'))
 
 bot.command('scheduleMute', (ctx) => {
+    const today = new Date()
+    const timeNow = `${today.getHours()} - ${today.getMinutes()} - ${today.getSeconds()}`
+
     // Check if user is admin
     if (!ADMINS.includes(ctx.from.id)) {
         return ctx.reply('Only admins can use this command')
@@ -27,6 +32,7 @@ bot.command('scheduleMute', (ctx) => {
 
     // Schedule chat block everyday at 12 PM
     blockChatJob = schedule.scheduleJob('0 0 * * *', () => {
+        ctx.reply('mở mõm' + timeNow)
         ctx.setChatPermissions({
             can_send_messages: false,
         })
@@ -34,6 +40,7 @@ bot.command('scheduleMute', (ctx) => {
 
     // Schedule chat unblock everyday at 6 AM
     unblockChatJob = schedule.scheduleJob('0 6 * * *', () => {
+        ctx.reply('khoá mõm' + timeNow)
         ctx.setChatPermissions({
             can_send_messages: true,
         })
