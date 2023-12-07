38b2f74f01f80a903ab352ba6bada6354e9e941d
DESKX300-PC\Admin <qwerty7415963@gmail.com>
2023-12-07 17:26:59 +0700

    + add chat permissions

--- bot.js
+++ bot.js
@@ -13,6 +13,18 @@ const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
 // Admin user IDs
 const ADMINS = process.env.ADMIN_ID
+const chatPermissions = {
+    can_send_messages: true,
+    can_send_photos: true,
+    can_send_media_messages: true,
+    can_send_polls: false,
+    can_send_audios: false,
+    can_send_video_notes: false,
+    can_send_voice_notes: false,
+    can_send_documents: false,
+    can_add_web_page_previews: false,
+    // can_send_other_messages: true, // This allows stickers and GIFs
+}
 
 app.get('*', function (req, res) {
     res.send('Live')
@@ -20,7 +32,31 @@ app.get('*', function (req, res) {
 
 bot.command('check', (ctx) => ctx.reply('is alive'))
 
+bot.command('lock', (ctx) =>
+    ctx.setChatPermissions({
+        can_send_messages: false,
+    })
+)
+bot.command('unlock', (ctx) =>
+    ctx.setChatPermissions({
+        can_send_messages: true,
+        can_send_photos: true,
+        can_send_media_messages: true,
+        can_send_polls: false,
+        can_send_audios: false,
+        can_send_video_notes: false,
+        can_send_voice_notes: false,
+        can_send_documents: false,
+        can_add_web_page_previews: false,
+        // can_send_other_messages: true, // This allows stickers and GIFs
+    })
+)
+
 bot.command('scheduleMute', (ctx) => {
+    const messagePermissions = {
+        can_send_messages: false,
+    }
+
     // Check if user is admin
     if (ADMINS !== ctx.from.id.toString()) {
         return ctx.reply('Only admins can use this command')
