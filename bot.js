cf0b8e4a7eb93f83e9a0bda64048f3dad34fa189
DESKTOP-E24A547\qwert <qwerty7415963@gmail.com>
2023-12-10 15:29:01 +0700

    + update chat permission and cron string

--- bot.js
+++ bot.js
@@ -13,18 +13,6 @@ const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
 // Admin user IDs
 const ADMINS = process.env.ADMIN_ID
-const chatPermissions = {
-    can_send_messages: true,
-    can_send_photos: true,
-    can_send_media_messages: true,
-    can_send_polls: false,
-    can_send_audios: false,
-    can_send_video_notes: false,
-    can_send_voice_notes: false,
-    can_send_documents: false,
-    can_add_web_page_previews: false,
-    // can_send_other_messages: true, // This allows stickers and GIFs
-}
 
 app.get('*', function (req, res) {
     res.send('Live')
@@ -32,31 +20,7 @@ app.get('*', function (req, res) {
 
 bot.command('check', (ctx) => ctx.reply('is alive'))
 
-bot.command('lock', (ctx) =>
-    ctx.setChatPermissions({
-        can_send_messages: false,
-    })
-)
-bot.command('unlock', (ctx) =>
-    ctx.setChatPermissions({
-        can_send_messages: true,
-        can_send_photos: true,
-        can_send_media_messages: true,
-        can_send_polls: false,
-        can_send_audios: false,
-        can_send_video_notes: false,
-        can_send_voice_notes: false,
-        can_send_documents: false,
-        can_add_web_page_previews: false,
-        // can_send_other_messages: true, // This allows stickers and GIFs
-    })
-)
-
 bot.command('scheduleMute', (ctx) => {
-    const messagePermissions = {
-        can_send_messages: false,
-    }
-
     // Check if user is admin
     if (ADMINS !== ctx.from.id.toString()) {
         return ctx.reply('Only admins can use this command')
@@ -72,17 +36,29 @@ bot.command('scheduleMute', (ctx) => {
     }
 
     // Schedule chat block everyday at 12 PM
-    blockChatJob = schedule.scheduleJob('0 0 * * *', () => {
+    blockChatJob = schedule.scheduleJob('0 0 0 * * ?', () => {
         ctx.setChatPermissions({
             can_send_messages: false,
         })
     })
 
     // Schedule chat unblock everyday at 6 AM
-    unblockChatJob = schedule.scheduleJob('0 6 * * *', () => {
-        ctx.setChatPermissions({
-            can_send_messages: true,
-        })
+    unblockChatJob = schedule.scheduleJob('0 0 6 * * ?', () => {
+        ctx.setChatPermissions(
+            {
+                can_send_messages: true,
+                can_send_photos: true,
+                can_send_other_messages: true,
+                can_send_audios: false,
+                can_send_videos: false,
+                can_send_documents: false,
+                can_send_video_notes: false,
+                can_send_video_notes: false,
+            },
+            {
+                use_independent_chat_permissions: true,
+            }
+        )
     })
 
     ctx.reply('Daily mute scheduled!')
